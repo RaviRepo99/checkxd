56f89ac3f43ace1dcbcbe986c651ef4e7e4d8b3a
miteshparadox <ravimandal.here@gmail.com>
2026-08-31 21:10:03 +0545

    Fix hosted donation database connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,11 @@
 import {drizzle} from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import {getDatabaseUrl} from '@/lib/env';
-const client = postgres(getDatabaseUrl(), {prepare: false});
+const client = postgres(getDatabaseUrl(), {
+	prepare: false,
+	ssl: 'require',
+	max: 1,
+	connect_timeout: 10,
+});
 
 export const db = drizzle({client});
